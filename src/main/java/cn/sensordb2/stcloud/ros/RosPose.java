461680ac057f2afd2cc9c891455e76294c1b99ca
leey <leey0410@qq.com>
2019-11-23 10:29:40 +0800

    first commit in v2 branch

--- src/main/java/cn/sensordb2/stcloud/ros/RosPose.java
+++ src/main/java/cn/sensordb2/stcloud/ros/RosPose.java
@@ -6,22 +6,29 @@ import edu.wpi.rail.jrosbridge.callback.TopicCallback;
 import edu.wpi.rail.jrosbridge.messages.Message;
 
 public class RosPose {
+    private String pose = null;
 
-    public static Pose POSE;
+    public void setPose(String pose) {
+        this.pose = pose;
+    }
 
-    public static void getPose() {
+    public String getPose() {
+        return pose;
+    }
+
+    public  void uavPose() {
 
         Ros ros = RosInstance.getInstance().getRos();
         Topic topic = new Topic(ros, "/firefly/ground_truth/pose", "geometry_msgs/Pose");
         topic.subscribe(new TopicCallback() {
             @Override
             public void handleMessage(Message message) {
-                POSE = new Pose(message);
+                pose = message.toJsonObject().toString();
                 topic.unsubscribe();
             }
         });
         try {
-            Thread.sleep(100);
+            Thread.sleep(10);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
